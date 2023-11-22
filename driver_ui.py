c8b4c4afdd9cec35ce04d411436fb2b6c1049b50
MrLeopard <fastorm@qq.com>
2023-11-22 09:53:05 +0800

    first init

---
+++ driver_ui.py
@@ -0,0 +1,146 @@
+"""
+Autor   : Mr Leopard
+License : MIT
+"""
+import time
+import copy
+from tkinter import Tk, Text, END, Scrollbar
+import webbrowser
+from intent_provider import Provider
+
+
+class NewUi(Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.wm_title("cmder")
+        # self.wm_attributes("-toolwindow", 2)
+        self.geometry('800x600')
+        self.bind("<Key>", self.on_key_event)
+        self.full_screen = False
+        self.attributes('-fullscreen', self.full_screen)
+
+        scrollbar = Scrollbar(self, orient="vertical")
+        scrollbar.pack(side="right", fill="y")
+
+        self.text_size = 12
+        self.font = "msyh"
+        self.text_area = Text(self, background="#323232", font=(self.font, self.text_size)
+                              , yscrollcommand=scrollbar.set)
+        self.text_area.pack(side="left", fill="both", expand=True)
+        scrollbar.config(command=self.text_area.yview)
+
+        self.provider = Provider()
+        self.text_color = "#D6D6D6"
+        self.window_content = None
+        self.insert(self.provider.get_dozen())
+
+    def switch_color(self):
+        if self.text_area["background"] == "#323232":
+            self.text_area["background"] = "#D6D6D6"
+            self.text_color = "#323232"
+
+        else:
+            self.text_area["background"] = "#323232"
+            self.text_color = "#D6D6D6"
+
+        # self.text_area.tag_configure(self.text_color, foreground=self.text_color)
+        # self.text_area.tag_add(self.text_color, "1.0", END)
+        # self.text_area.config(fg=self.text_color)
+        # self.text_area.see(END)
+        # self.insert_titles(self.provider.get_dozen("current"))
+
+        self.insert(self.window_content)
+
+    def clear_text(self):
+        self.text_area.delete("1.0", END)
+
+    def insert(self, content):
+        self.clear_text()
+        self.window_content = content
+        if type(content) == list:
+            n = 65
+            for i in content:
+                self.text_area.tag_configure(self.text_color, foreground=self.text_color)
+                self.text_area.insert(END, f"{chr(n)}、 {i['title']}---[{i['time']}]\n\n", self.text_color)
+                self.text_area.see(END)
+                n += 1
+
+        elif type(content) == dict:
+            self.clear_text()
+            self.text_area.insert(END, f'【{content["title"]}】\n', self.text_color)
+            self.text_area.insert(END, f'{content["time"]} {content["url"]}  \n\n', self.text_color)
+            for t in self.provider.get_new_content_text(content):
+                self.text_area.insert(END, f'{t}\n', self.text_color)
+        # self.text_area.see(END)
+
+    def insert_content(self, key):
+        s = self.window_content[key - 65]
+        self.clear_text()
+        self.insert(s)
+
+    def change_news(self, direction=""):
+        step = 0
+        if direction == "Next":
+            step = 1
+        elif direction == "Prior":
+            step = -1
+        __current_news = copy.deepcopy(self.window_content)
+        # print(self.provider.get_dozen("current"))
+        __news_index = self.provider.get_dozen("current").index(__current_news)
+        point_index = __news_index + step
+        if point_index < 0:
+            if self.provider.content["index"] == 0:
+                pass
+            else:
+                self.insert(self.provider.get_dozen("previous")[-1])
+
+        elif point_index >= len(self.provider.get_dozen("current")):
+            self.insert(self.provider.get_dozen("next")[0])
+        else:
+            self.insert(self.provider.get_dozen("current")[point_index])
+
+    def on_key_event(self, event):
+        # print(event.keycode, event.keysym)
+        if event.keysym == "equal":
+            if self.text_size < 60:
+                self.text_size += 1
+                self.text_area.configure(font=(self.font, self.text_size))
+                time.sleep(0.1)
+        elif event.keysym == "minus":
+            if self.text_size > 2:
+                self.text_size -= 1
+                self.text_area.configure(font=(self.font, self.text_size))
+            time.sleep(0.1)
+        elif event.keysym == "slash":
+            self.switch_color()
+        elif event.keysym == "Right" and type(self.window_content) == list:
+            self.insert(self.provider.get_dozen("next"))
+        elif event.keysym == "Left" and type(self.window_content) == list:
+            self.insert(self.provider.get_dozen("previous"))
+        elif type(self.window_content) == list and \
+                event.keycode in range(65, len(self.window_content) + 65):
+            self.insert_content(event.keycode)
+        elif event.keysym == "BackSpace":
+            self.insert(self.provider.get_dozen("current"))
+        elif type(self.window_content) == dict and event.keysym in ["b", "B"]:
+            webbrowser.open(self.window_content["url"])
+
+        elif type(self.window_content) == dict and event.keysym == "Down":
+            self.text_area.yview_scroll(1, "unit")
+        elif type(self.window_content) == dict and event.keysym == "Up":
+            self.text_area.yview_scroll(-1, "unit")
+
+        elif type(self.window_content) == dict and event.keysym == "Next":
+            self.change_news("Next")
+
+        elif type(self.window_content) == dict and event.keysym == "Prior":
+            self.change_news("Prior")
+
+        elif event.keysym == "0":
+            self.full_screen = not self.full_screen
+            self.attributes('-fullscreen', self.full_screen)
+
+
+if __name__ == "__main__":
+    nu = NewUi()
+    nu.mainloop()
---
+++ readme.txt
@@ -0,0 +1,29 @@
+1.依赖
+windows
+pip install requests bs4
+
+linux & mac
+pip3 install requests bs4
+如linux无pip，需sudo pip install python3-pip
+
+2.运行
+python3 driver_ui.py
+
+3.快捷键（字母不分大小写）
+	全局
+	- 缩小字体
+	=  加大字体
+	/  切换黑白主题
+
+	新闻列表
+	left 上一页
+	right 下一页
+	字母[a-o]  打开对应的新闻
+
+	新闻正文
+	UP  滚动条上
+	Down 滚动条下
+	B 启动浏览器查阅此新闻
+	Backspace 返回新闻列表
+	pageup 上一篇新闻
+	pageDown 下一篇新闻
